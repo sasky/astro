362accfd56631653c029626ea4982803206cfa86
sasky <sasky.creative@gmail.com>
2025-05-19 16:14:52 +1200

    works but...weird

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -9,3 +9,16 @@ export function toAnchor(urlPath) {
   const str = urlPath.charAt(0) === "/" ? urlPath.substring(1) : urlPath;
   return str.trim().replace(/\//g, "-");
 }
+
+/**
+ * Checks if the given anchor string ends with a dash
+ *
+ * @param {string} anchor - The anchor string to check.
+ * @return {boolean} Returns true if the anchor ends with a dash, false otherwise.
+ */
+export function endsWithDash(anchor) {
+  if (!anchor || typeof anchor !== "string") {
+    return false;
+  }
+  return anchor.endsWith("-");
+}
